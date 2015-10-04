1dadabfc59c95316b7aabac5779ea24a6e1a0b30
olivier <guerin.olivier@outlook.com>
2015-10-04 19:49:25 +0200

    [v1.2.7] update extract

--- extractFunctionName.js
+++ extractFunctionName.js
@@ -26,13 +26,13 @@
         var anonymous = '#Anonymous', extract;
         var functionToString = Function.prototype.toString;
 
-        if(functionToString.call(new (  function ie_proof /**/ () {})().constructor) === functionToString.call(function ie_proof(){})){ //if >= IE 9
+        if(functionToString.call(new (  function ie_proof /**/ (){})().constructor) === 'function ie_proof(){}'){ //if >= IE Edge
             extract =  function (value) {
-                var start_index = (value[0]==='f')?9:10; //fix bug on IE < edge (v[0]: '\n')
+                var start_index = 9;
                 var end_index = value.indexOf('(', start_index);
                 return (start_index === end_index)? anonymous : value.slice(start_index, end_index);
             }
-        } else {
+        } else { //IE !== Edge
             var commentBlocRegexp = '\\/\\*(?:.|[\\r\\n])*?\\*\\/';
             var commentLinearRegexp = '\\/\\/.*?\\n';
             var commentRegex = '(?:'+commentBlocRegexp+'|'+commentLinearRegexp+'|'+'\\s'+')*';
